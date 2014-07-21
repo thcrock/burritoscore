2e8fc4b1c080e0e5bc9e3b1c859aaa57d04f1001
Eric Burns <ericjosephburns@gmail.com>
2014-07-20 22:16:35 -0500

    backfilled geocoded data for businesses

--- burritoscore/burritoscore/views.py
+++ burritoscore/burritoscore/views.py
@@ -1,8 +1,10 @@
 import json
+from multiprocessing import Pool
 
 from django.conf import settings
 from django.shortcuts import render
 from django.http import HttpResponse, Http404
+from geopy.geocoders import GoogleV3
 from burritoscore.scorers.binscorer import BinScorer
 
 
@@ -16,7 +18,24 @@ def home(request):
 	return render(request, 'burritoscore/home.html', context)
 
 
+def geolocate_business(business):
+	"""
+	If the business doesn't have coordinates, let's get them
+	from the address.
+	"""
+	location = ' '.join(business['location']['display_address'])
+
+	geolocator = GoogleV3(api_key=settings.GOOGLE_MAPS_API_KEY)
+	address, (latitude, longitude) = geolocator.geocode(location)
+	return (latitude, longitude)
+
+
 def format_business(business):
+	# Backfill data if it's not there and we need it.
+	if 'coordinate' not in business['location']:
+		lat, lng = geolocate_business(business)
+		business['location']['coordinate'] = {'latitude': lat, 'longitude': lng}
+
 	return {
 		'lat': business['location']['coordinate']['latitude'],
 		'lon': business['location']['coordinate']['longitude'],
@@ -31,7 +50,9 @@ def get_score_by_location(request, location):
 	if request.is_ajax():
 		scorer = BinScorer()
 		score, businesses = scorer.score(location)
-		formatted_businesses = [format_business(business) for business in businesses.values() if 'coordinate' in business['location']]
+
+		pool = Pool()
+		formatted_businesses = pool.map(format_business, businesses.values())
 
 		return HttpResponse(json.dumps({'score': score, 'businesses': formatted_businesses}), content_type="application/json")
 
